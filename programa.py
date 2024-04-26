33b8cf6dc47563755e7cad637e3251c98d4024d8
Kevin Jacome <kjacomeg@est.ups.edu.ec>
2024-04-26 15:36:56 -0500

    Resulto todos los ejercicios

--- programa.py
+++ programa.py
@@ -43,13 +43,13 @@ import math
 
 def area_circulo(radio):
     area = math.pi * radio ** 2  # Se calcula el área del círculo utilizando la fórmula matemática
-    return str(area)# Se devuelve el área calculada
+    return area# Se devuelve el área calculada
 
 if __name__ == "__main__":
     radio = float(input("Ingrese el radio del círculo: "))  # Se solicita al usuario que ingrese el radio del círculo
     print("El área del circulo es: "+ area_circulo(radio))# Se imprime el área calculada del círculo
 
-# 6 Define una función que reciba dos números como argumentos y devuelva su suma.
+#6 Define una función que reciba dos números como argumentos y devuelva su suma.
 def suma(a, b):
    x=a+b 
    return x# Se devuelve la suma de los dos números recibidos como argumentos
@@ -59,26 +59,27 @@ if __name__ == "__main__":
     num2 = int(input("Ingrese el segundo número: "))# Se solicita al usuario que ingrese el segundo número
     print("La suma es:", suma(num1, num2))  # Se imprime la suma de los dos números ingresados
 
-# # 7 Modifica la función que calcula el área del círculo para que reciba el radio como parámetro.
-# import math
+#7 Modifica la función que calcula el área del círculo para que reciba el radio como parámetro.
+import math
 
-# def area_circulo(radio):
-#     area = math.pi * radio ** 2  # Se calcula el área del círculo utilizando la fórmula matemática
-#     # Se devuelve el área calculada
+def area_circulo(radio):
+    pim=math.pi
+    area = pim* radio**2 # Se calcula el área del círculo utilizando la fórmula matemática
+    return area# Se devuelve el área calculada
 
-# if __name__ == "__main__":
-#     radio = float(input("Ingrese el radio del círculo: "))  # Se solicita al usuario que ingrese el radio del círculo
-#     print("El área del círculo es:", area_circulo(radio))  # Se imprime el área calculada del círculo
+if __name__ == "__main__":
+    radio = float(input("Ingrese el radio del círculo: "))  # Se solicita al usuario que ingrese el radio del círculo
+    print("El área del círculo es:" + area_circulo(radio))  # Se imprime el área calculada del círculo
 
-# # 8 Diseña un programa que convierta grados Celsius a Fahrenheit y viceversa, utilizando funciones para realizar los cálculos.
-# def celsius_a_fahrenheit(celsius):
-#     return (celsius * 9/5) + 32  # Se aplica la fórmula de conversión de Celsius a Fahrenheit
+# 8 Diseña un programa que convierta grados Celsius a Fahrenheit y viceversa, utilizando funciones para realizar los cálculos.
+def celsius_a_fahrenheit(celsius):
+    return (celsius * 9/5) + 32  # Se aplica la fórmula de conversión de Celsius a Fahrenheit
 
-# def fahrenheit_a_celsius(fahrenheit):
-#     # Se aplica la fórmula de conversión de Fahrenheit a Celsius
+def fahrenheit_a_celsius(fahrenheit):
+    return (fahrenheit-32) * 5/9# Se aplica la fórmula de conversión de Fahrenheit a Celsius
 
-# if __name__ == "__main__":
-#     celsius = float(input("Ingrese la temperatura en grados Celsius: "))  # Se solicita al usuario que ingrese la temperatura en grados Celsius
-#     print("Temperatura en Fahrenheit:", celsius_a_fahrenheit(celsius))  # Se imprime la temperatura convertida a grados Fahrenheit
-#     # Se solicita al usuario que ingrese la temperatura en grados Fahrenheit
-#     print("Temperatura en Celsius:", fahrenheit_a_celsius(fahrenheit))  # Se imprime la temperatura convertida a grados Celsius
+if __name__ == "__main__":
+    celsius = float(input("Ingrese la temperatura en grados Celsius: "))  # Se solicita al usuario que ingrese la temperatura en grados Celsius
+    print("Temperatura en Fahrenheit:", celsius_a_fahrenheit(celsius))  # Se imprime la temperatura convertida a grados Fahrenheit
+    fahrenheit = float(input("Ingrese la temperatura en grados fahrenheit: "))# Se solicita al usuario que ingrese la temperatura en grados Fahrenheit
+    print("Temperatura en Celsius:", fahrenheit_a_celsius(fahrenheit))  # Se imprime la temperatura convertida a grados Celsius
